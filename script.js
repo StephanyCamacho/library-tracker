3cb735aa814c7169ae21bc1d0714cd78fa36f849
Stephany Camacho-Lara <stephany_c19@yahoo.com>
2020-05-28 19:00:32 -0600

    functionality working

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-let myLibrary = [];
-
 const myLibrary = findLibrary('library');
 const bookTable = document.querySelector('.book-list');
 const addButton = document.querySelector('.submit-button');
-const BOOK_INPUT = document.querySelectorAll('.book-info input');
-const EMPTY_INSTANCE = bookTable.querySelector('.empty-list');
-addButton.addEventListener('click', addBook);
-
-function Book(title, author, pages, read) {
-    // the constructor...
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+const bookInfo = document.querySelectorAll('.book-info input');
+const emptyList = bookTable.querySelector('.empty-list');
+addButton.addEventListener('click', addBookToLibrary);
 
 function addBookToLibrary() {
-    // do stuff here
-
+	const Book = new BookConstructor(bookInfo[0].value, bookInfo[1].value, parseInt(bookInfo[2].value),
+		bookInfo[3].value === 'yes' ? true : false);
+	for (let book of bookInfo) {
+		book.value = '';
+	}
+	myLibrary.push(Book);
+	buildLibrary();
 }
+
+let deleteBook = class {
+	handleEvent(event) {
+		myLibrary.splice(event.currentTarget.dataset.index, 1);
+		buildLibrary();
+	}
+}
+const removeBook = new deleteBook();
+
+function BookConstructor(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+function buildLibrary() {
+	bookTable.querySelector('tbody').innerHTML = '';
+	let index = 0;
+	for (let book of myLibrary) {
+		const newBookRow = document.createElement('tr');
+		newBookRow.innerHTML = `
+		<td>${book.title}</td>
+		<td>${book.author}</td>
+		<td>${book.pages}</td>
+		<td>${book.read === false ? 'not read yet' : 'Read'}</td>
+		<td><button type="button" data-index=${index} class="delete-book button-base">Delete book</button></td>
+
+		`;
+		bookTable.querySelector('tbody').append(newBookRow);
+		document.querySelector(`.delete-book[data-index="${index}"]`).addEventListener('click', removeBook);
+		++index;
+	}
+	saveBook(myLibrary);
+	if (!bookTable.querySelector('tbody').innerHTML) {
+		bookTable.querySelector('tbody').append(emptyList);
+	}
+}
+
+function saveBook(library) {
+	localStorage.setItem('library', JSON.stringify(library));
+}
+
+function findLibrary(key) {
+	if (!localStorage.getItem(key)) {
+		return [];
+	}
+	return JSON.parse(localStorage.getItem(key));
+}
+buildLibrary();
